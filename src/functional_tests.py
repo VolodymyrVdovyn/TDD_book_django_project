08af66b566257d8a1dc77a024dea256180421d57
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-06-22 12:34:50 +0300

    Update first FT, now using unittest

--- src/functional_tests.py
+++ src/functional_tests.py
@@ -1,8 +1,41 @@
-from django.test import TestCase
+from logging import warning
 from selenium import webdriver
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
+    """Test for new visitor"""
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'worked' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Open prjoect home page
+        self.browser.get('http://localhost:8000')
+
+        # Home page title says that it is To-Do project
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Complete test!")
+
+        # You can write first todo line
+
+        # Write "Buy milk"
+
+        # When click Enter. On page we have new line "1: Buy milk"
+
+        # On page you can write another todo line
+        # Write "Make milkshake"
+
+        # Page reload again and show two todo line
+
+        # Site must generate uniq URL for user and show some text with information about that
+
+        # When click on that URL user can see him list todo
+
+        # End test session
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
+
