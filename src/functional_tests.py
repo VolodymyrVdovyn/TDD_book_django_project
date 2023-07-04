9039d9c16466af415471f170c147746d316c69e2
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-07-04 17:29:06 +0300

    FT now check if you can write new elements in list

--- src/functional_tests.py
+++ src/functional_tests.py
@@ -1,7 +1,10 @@
+import time
 import unittest
 from logging import warning
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,22 +22,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # Home page title says that it is To-Do project
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Complete test!")
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
-        # You can write first todo line
+        # You can write first To-Do line
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEquals(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
         # Write "Buy milk"
+        input_box.send_keys("Buy milk")
 
-        # When click Enter. On page we have new line "1: Buy milk"
+        # When click Enter. On page, we have new line "1: Buy milk"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # On page you can write another todo line
+        table = self.browser.find_element(By.TAG_NAME, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Buy milk" for row in rows))
+
+        # On page, you can write another To-Do line
         # Write "Make milkshake"
+        self.fail("Complete test!")
 
-        # Page reload again and show two todo line
+        # Page reload again and show two To-Do line
 
         # Site must generate uniq URL for user and show some text with information about that
 
-        # When click on that URL user can see him list todo
+        # When click on that URL user can see him list To-Do
 
         # End test session
 
