543a73c5ec7042b58047935ad24485f3ed344d26
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-09-19 19:13:35 +0300

    Add model and tests

--- src/apps/lists/tests.py
+++ src/apps/lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -8,23 +8,34 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "First item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEquals(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEquals(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
         self.assertEquals(first_saved_item.text, "First item")
+        self.assertEquals(first_saved_item.list, list_)
+
+        second_saved_item = saved_items[1]
         self.assertEquals(second_saved_item.text, "Second item")
+        self.assertEquals(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -33,8 +44,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_display_all_items(self):
-        Item.objects.create(text="First item")
-        Item.objects.create(text="Second item")
+        list_ = List.objects.create()
+        Item.objects.create(text="First item", list=list_)
+        Item.objects.create(text="Second item", list=list_)
 
         response = self.client.get("/lists/unique-list")
 
