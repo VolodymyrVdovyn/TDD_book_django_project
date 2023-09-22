e25e17f115f8324cd606fd2613c2f7af7b5f9dbc
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-09-22 14:20:49 +0300

    Add test_layout_and_styling functional test

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -97,6 +97,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("Make lunch", page_text)
         self.assertNotIn("Buy milk", page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(input_box.location['x'] + (input_box.size['width'] / 2), 512, delta=10)
+
+        input_box.send_keys("Testing")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Testing")
+
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(input_box.location['x'] + (input_box.size['width'] / 2), 512, delta=10)
+
+
+
 
 
 
