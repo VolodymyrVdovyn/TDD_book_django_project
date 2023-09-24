8c4b96d7c127a22502bf2d935332a1a0b5ea3e6e
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-09-24 15:41:09 +0300

    Add STATIC_ROOT and deactivate admin app Add collected_static dir to gitignore

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,7 @@ from lists import urls as list_urls
 from lists import views as list_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", list_views.home_page, name="home"),
     path("lists/", include(list_urls)),
 ]
