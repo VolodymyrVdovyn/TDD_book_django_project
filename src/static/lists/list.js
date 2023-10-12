dc72f15119bb4b0e24d58c13ece48897457e17f1
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-10-12 22:00:56 +0300

    change event from keypress and click

--- src/static/lists/list.js
+++ src/static/lists/list.js
@@ -1,6 +1,6 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
-    $('input[name="text"]').on("click", function () {
+    $('input[name="text"]').on("keypress click", function () {
         $(".has-error").hide();
     });
 };
