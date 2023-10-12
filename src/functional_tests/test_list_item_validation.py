09d6bf86dec0a9495ed20ad358a5291470f41c44
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-10-12 18:55:24 +0300

    Add test_error_messages_are_cleared_on_input FT

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -6,6 +6,10 @@ from lists.forms import DUPLICATE_ITEM_ERROR
 
 
 class ItemValidationTest(FunctionalTest):
+    @property
+    def get_error_element(self):
+        return self.browser.find_element(By.CSS_SELECTOR, ".has-error")
+
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
 
@@ -35,8 +39,18 @@ class ItemValidationTest(FunctionalTest):
 
         self.get_item_input_box().send_keys("Buy milk")
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for(
-            lambda: self.assertEqual(
-                self.browser.find_element(By.CSS_SELECTOR, ".has-error").text, DUPLICATE_ITEM_ERROR
-            )
-        )
+        self.wait_for(lambda: self.assertEqual(self.get_error_element.text, DUPLICATE_ITEM_ERROR))
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.live_server_url)
+
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        self.get_item_input_box().send_keys("Buy milk")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertTrue(self.get_error_element.is_displayed()))
+
+        self.get_item_input_box().send_keys("a")
+        self.wait_for(lambda: self.assertFalse(self.get_error_element.is_displayed()))
