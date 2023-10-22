193992cd822b10812ac6d6240cadeeb783838875
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-10-22 15:07:31 +0300

    Add tests for check if lists have owners

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import get_user_model, BACKEND_SESSION_KEY, SESSION_KEY
 from django.conf import settings
-from django.contrib.sessions.backends.db import SessionStore
-from functional_tests.server_tools import create_session_on_server
-from functional_tests.management.commands.create_session import create_pre_authenticated_session
+from django.contrib.auth import get_user_model
+from selenium.webdriver.common.by import By
 
 from functional_tests.base import FunctionalTest
+from functional_tests.management.commands.create_session import create_pre_authenticated_session
+from functional_tests.server_tools import create_session_on_server
 
 User = get_user_model()
 
@@ -22,10 +22,29 @@ class MyListsTest(FunctionalTest):
         self.browser.add_cookie(dict(name=settings.SESSION_COOKIE_NAME, value=session_key, path="/"))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(TEST_EMAIL)
-
         self.create_pre_authenticated_session(TEST_EMAIL)
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(TEST_EMAIL)
 
+        self.add_list_item("First list item")
+        self.add_list_item("Second list item")
+
+        first_list_url = self.browser.current_url
+
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "First list item"))
+        self.browser.find_element(By.LINK_TEXT, "First list item").click()
+        self.wait_for(lambda: self.assertEqual(self.browser.current_url, first_list_url))
+
+        self.browser.get(self.live_server_url)
+        self.add_list_item("Another list item")
+        second_list_url = self.browser.current_url
+
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Another list item"))
+        self.browser.find_element(By.LINK_TEXT, "Another list item").click()
+        self.wait_for(lambda: self.assertEqual(self.browser.current_url, second_list_url))
+
+        self.browser.find_element(By.LINK_TEXT, "Log out").click()
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element(By.LINK_TEXT, "My lists"), []))
