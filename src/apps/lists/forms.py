7ae976920ac54a2a02be905bc462c4acdc7bd566
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2023-10-22 17:47:21 +0300

    Remove own save logic from forms

--- src/apps/lists/forms.py
+++ src/apps/lists/forms.py
@@ -16,10 +16,6 @@ class ItemForm(forms.models.ModelForm):
         }
         error_messages = {"text": {"required": EMPTY_ITEM_ERROR}}
 
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
-
 
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
@@ -32,6 +28,3 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {"text": [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-
-    def save(self):
-        return forms.models.ModelForm.save(self)
